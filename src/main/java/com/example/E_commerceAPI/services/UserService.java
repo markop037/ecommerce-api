cc09596820127e0d141d8318a4222e8d549fc7a2
Marko Peric <marko.peric2102@gmail.com>
2024-12-23 23:42:58 +0100

    Fix SQL order overlap, add password hashing, and configure securit

--- src/main/java/com/example/E_commerceAPI/services/UserService.java
+++ src/main/java/com/example/E_commerceAPI/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.E_commerceAPI.services;
 import com.example.E_commerceAPI.models.User;
 import com.example.E_commerceAPI.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,8 +12,11 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public User registerUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
